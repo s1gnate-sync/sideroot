c2fff2b74850ad18a2c47ce4bb9beedb0b3b91d6
s1gnate-sync <139636216+s1gnate-sync@users.noreply.github.com>
2023-12-02 13:16:11 +0000

    Update sideroot.c

--- sideroot.c
+++ sideroot.c
@@ -1,6 +1,8 @@
 #define _GNU_SOURCE
-
 #include <err.h>
+#include <errno.h>
+#include <fmtmsg.h>
+#include <getopt.h>
 #include <grp.h>
 #include <limits.h>
 #include <pwd.h>
@@ -11,234 +13,265 @@
 #include <sys/mount.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
-#include <getopt.h>
-
-#include <errno.h>
-#define _COLOR_START_RED  "\x1B[31m"
-
-#define _COLOR_STOP  "\x1B[0m"
-
-#define DIE(expr) { \
-	int error = errno; \
-	printf(\
-		"%s%s:%s:%i: error in %s: %s%s\n",  \
-		_COLOR_START_RED, prog, __FILE__ ,__LINE__ , \
-		#expr, strerror(error), _COLOR_STOP \
-	); \
-	exit(error); \
-}
-
-#define TRY_SYSCALL(expr) if (expr < 0) DIE(expr)
-
-#define bind(src, dst) mount(src, dst, "bind", MS_BIND | MS_PRIVATE, NULL)
-
-static char *prog;
-const char *sep = ":";
-
-void help() {
-	printf("\nUsage:\n %s [options] -- [<program> [<argument>...]]\n\n", prog);	
-	printf("Options:\n");
-	printf(" -r <root-dir>    Change root directory.\n");
-	printf(" -d <ch-dir>      Set working directory inside chroot (defaults to /).\n");
-	printf(" -u <uid>         Change user inside chroot.\n");
-	printf(" -g <gid>         Change group inside chroot.\n");
-	printf(" -b <src<%s<dst>> Bind <src> to <root-dir>/<dst>.\n", sep);
-	printf("                       Multiple instances allowed.\n");
-	printf(" -E               Start with empty environment.\n");
-	printf(" -e <name>=<val>  Set environment variable <name> to <val>.\n");
-	printf("                       Multiple instances allowed.\n");	
-	printf(" -h               Display this help.\n");
-	printf(" --chroot         Shortcut for --proc --sys --dev --devpts.\n");
-	printf(" --proc           Mount proc filesystem to /proc.\n");
-	printf(" --sys            Mount sysfs filesystem to /sys.\n");
-	printf(" --dev            Mount devtmpfs filesystem to /dev.\n");
-	printf(" --devpts         Mount devpts filesystem to /dev/pts. Implies --dev.\n");
-	printf(" --run            Mount tmpfs filesystem to /run.\n");
-	printf(" --tmp            Mount tmpfs filesystem to /tmp.\n");
-	printf("\nRootless mode requires file capabilities to be set:\n");
-	printf(" sudo setcap =ep \"<prog_path>\"\n");
-}
-
-#define BIND_LIMIT 100
-struct bind_mount {
-	char src[PATH_MAX];
-	char dst[PATH_MAX];
-};
 
 #define ENV_LIMIT 100
 #define ENV_VAR_MAX 1000
+#define BIND_LIMIT 100
+#define DIR_SEP ":"
+#define FMT_OPT(spec, desc) " " spec "\n " desc "\n\n"
 
-int main(int argc, char* argv[]) {
-	prog = argv[0];
+void die(const char *msg) {
+  if (errno)
+    perror(msg);
+  else
+    fprintf(stderr, "%s\n", msg);
+  exit(1);
+}
 
-	char *root_dir = "/";
-	char *ch_dir = "/";
-	int uid = -1;
-	int gid = -1;
-	int proc = 0;
-	int sys = 0;
-	int dev = 0;
-	int devpts = 0;
-	int run = 0;
-	int tmp = 0;
-	int clear_env = 0;
+typedef struct {
+  char src[PATH_MAX];
+  char dst[PATH_MAX];
+} bind_mount;
 
-	struct bind_mount binds[BIND_LIMIT];
-	int bind_count = 0;
+void help() {
+  printf("Usage:\n sideroot [options] -- [<program> [<argument>...]]\n\n");
+  printf(
+      "Options:\n" 
+   		FMT_OPT("-r <root-dir>", "Set root directory.")
+		FMT_OPT("-d <ch-dir>", "Set working directory for chroot.") 
 
-	char env[ENV_LIMIT][ENV_VAR_MAX];
-	int env_count = 0;
+		FMT_OPT("-D",
+		"Bind current directory to working directory in chroot")
 
-	if (argc == 1) {
-		help();
-		return 1;
-	}
+		FMT_OPT("-u <uid>", "Change user inside chroot") 
 
-	opterr = 0;
-	int arg;
-	while ((arg = getopt(argc, argv, ":hr:d:u:g:-::b:e:E")) != -1) {
-		switch(arg) {
-			case 'h':
-				help();
+		FMT_OPT("-g <gid>", "Change group inside chroot")
 
-				return 0;
-				
-			// root dir
-			case 'r':
-				root_dir = optarg;
-				
-				break;
-				
-			// working dir inside chroot
-			case 'd':
-				ch_dir = optarg;
-				
-				break;
+		FMT_OPT("-R", "Change user and group to 0") 
 
-			// uid
-			case 'u':
-				uid = atoi(optarg);
-				
-				break;
+		FMT_OPT("-b <src<" DIR_SEP "<dst>>",
+		"Bind <src> to <root-dir>/<dst>.")
 
-			// gid
-			case 'g':
-				gid = atoi(optarg);
-				
-				break;
+		FMT_OPT("-p", "Execute command as init") 
+		FMT_OPT("-U", "Change uid and gid to 1000")
 
-			// binds -b /usr/locall
-			case 'b':
-				char *src = strtok(optarg, sep);	
-				char *dst = strtok(NULL, sep);
+		FMT_OPT("-R", "Change uid and gid to 0") 
+		FMT_OPT("-E", "Do not clean environment.")
 
-				if (dst == NULL)
-					dst = src;
+		FMT_OPT("-e <name>=<val>",
+		"Set environment variable <name> to <val>.")
 
-				strcpy(binds[bind_count].src, src);					 
-				strcpy(binds[bind_count].dst, dst);
-					
-				bind_count++;
-				
-				break;
+		FMT_OPT("-h", "Display this help.") 
+		FMT_OPT("--noproc", "Do not mount proc.")
+	);
+}
 
-			case 'E':
-				clear_env = 1;
-				break;
+int main(int argc, char *argv[]) {
+  char wd_dir[PATH_MAX];
+  getcwd(wd_dir, sizeof(wd_dir));
 
-			case 'e':
-				if (strlen(optarg) < ENV_VAR_MAX) {
-					strcpy(env[env_count], optarg);
-					env_count++;
-				}
-				break;
+  char *root_dir = NULL;
+  char *ch_dir = "/";
+  int uid = getuid();
+  int gid = getgid();
+  int proc = 1;
+  int sys = 1;
+  int dev = 1;
+  int devpts = 1;
+  int run = 1;
+  int tmp = 1;
+  int clear_env = 1;
+  bind_mount binds[BIND_LIMIT];
+  int bind_count = 0;
+  int bind_wd = 0;
+  char env[ENV_LIMIT][ENV_VAR_MAX];
+  int env_count = 0;
 
-			// flags
-			case '-':	
-				int val = 1;
+  if (argc == 1) {
+    help();
+    return 1;
+  }
 
-				if (strcmp(optarg, "chroot") == 0) 
-					proc = sys = dev = devpts = val;
-				else if (strcmp(optarg, "proc") == 0) 
-					proc = val;
-				else if (strcmp(optarg, "sys") == 0) 
-					sys = val;
-				else if (strcmp(optarg, "dev") == 0) 
-					dev = val;
-				else if (strcmp(optarg, "devpts") == 0) 
-					dev = devpts = val;
-				else if (strcmp(optarg, "run") == 0) 
-					run = val;
-				else if (strcmp(optarg, "tmp") == 0) 
-					tmp = val;
-																				
-				break;
-			
-			case '?':
-				printf("%s%s: unrecognized option: %c%s\n", _COLOR_START_RED, prog, optopt, _COLOR_STOP);
-				help();
-				return 1;
-							
-			case ':':
-				printf("%s%s: option requires an argument: %c%s\n", _COLOR_START_RED, prog, optopt, _COLOR_STOP);
-				help();
-				return 1;
-		}
-	}
+  opterr = 0;
+  int arg;
+  while ((arg = getopt(argc, argv, "hpURr:d:Du:g::b:e:E")) != -1) {
+    switch (arg) {
+    case 'h':
+      help();
+      return 0;
+    case 'R':
+      uid = gid = 0;
+      break;
+    case 'U':
+      uid = gid = 1000;
+      break;
+    case 'r':
+      root_dir = optarg;
+      break;
+    case 'D':
+      bind_wd = 1;
+      if (!strcmp(ch_dir, "/")) ch_dir = "/work";
+      break;
+    case 'd':
+      ch_dir = optarg;
+      break;
+    case 'u':
+      uid = atoi(optarg);
+      break;
+    case 'g':
+      gid = atoi(optarg);
+      break;
+    case 'b':
+      char *src = strtok(optarg, DIR_SEP);
+      char *dst = strtok(NULL, DIR_SEP);
+      strcpy(binds[bind_count].src, src);
+      strcpy(binds[bind_count].dst, dst ? dst : src);
+      bind_count++;
+      break;
 
+    case 'E':
+      clear_env = 0;
+      break;
 
-	if (uid < 0) 
-		uid = getuid();
-		
-	if (gid < 0) 
-		gid = getgid();	
+    case 'e':
+      if (strlen(optarg) < ENV_VAR_MAX) {
+        strcpy(env[env_count], optarg);
+        env_count++;
+      }
+      break;
+    case '?':
+    case ':':
+      fprintf(stderr, "Invalid option -%c\n", optopt);
+      exit(1);
+    }
+  }
 
-	TRY_SYSCALL(unshare(CLONE_NEWNS | CLONE_FS | CLONE_THREAD))
-	
-	for (int index = 0; index < bind_count; index++) {
-		char *target;
-		asprintf(&target, "%s/%s", root_dir, binds[index].dst);					
-		TRY_SYSCALL(bind(binds[index].src, target))
-	}
+  int wd_index = -1;
+  if (bind_wd) {
+    strcpy(binds[bind_count].src, wd_dir);
+    strcpy(binds[bind_count].dst, ch_dir);
+    wd_index = bind_count;
+    bind_count++;
+  }
 
-	if (strcmp(root_dir, "/") != 0) {
-		TRY_SYSCALL(chroot(root_dir))
- 		TRY_SYSCALL(bind("/", "/"))
-		TRY_SYSCALL(chdir("/"))
- 	}
- 	
-	if (proc)
-		TRY_SYSCALL(mount("proc", "/proc", "proc", 0, NULL)) 	
-		
- 	if (dev) 
- 		TRY_SYSCALL(mount("devtmpfs", "/dev", "devtmpfs", 0, NULL))
- 		
-	if (devpts)
-		TRY_SYSCALL(mount("devpts", "/dev/pts", "devpts", 0, NULL))
-		
-	if (sys)
-		TRY_SYSCALL(mount("none", "/sys", "sysfs", 0, NULL))
-		
-	if (tmp)
-		TRY_SYSCALL(mount("none", "/tmp", "tmpfs", 0, NULL))
-		
-	if (run) 
-		TRY_SYSCALL(mount("none", "/run", "tmpfs", 0, NULL))
+  strcpy(binds[bind_count].src, "/var/run/chrome");
+  strcpy(binds[bind_count].dst, "/var/chrome");
+  bind_count++;
 
-  	TRY_SYSCALL(setgid(gid))
-  	TRY_SYSCALL(setuid(uid))
+  if (!root_dir)
+    root_dir = wd_dir;
 
-	if (clear_env) 
-		clearenv();
+  if (unshare(CLONE_NEWNS | CLONE_NEWPID | CLONE_FS | CLONE_THREAD))
+    die("unshare");
+  struct stat sb;
 
-	for (int index = 0; index < env_count; index++)
-		TRY_SYSCALL(putenv(env[index]))
+  for (int index = 0; index < bind_count; index++) {
+    char *target;
+    asprintf(&target, "%s/%s", root_dir,
+             binds[index].dst[0] == '/' ? binds[index].dst + 1
+                                        : binds[index].dst);
 
-  	TRY_SYSCALL(chdir(ch_dir))
+    if ((index == wd_index ||
+         strcmp(binds[index].src, "/var/run/chrome") == 0) &&
+        stat(target, &sb) && mkdir(target, S_IRWXU | S_IRWXG))
+      die(target);
 
-  	if (argc > optind) 
-  		TRY_SYSCALL(execvp(argv[optind], &argv[optind]))
-  		
-  	TRY_SYSCALL(execlp("sh", "sh", NULL))
+    if (mount(binds[index].src, target, "bind", MS_BIND | MS_PRIVATE, NULL))
+      die(target);
+    free(target);
+  }
+
+  int pid = fork();
+  if (pid == -1)
+    die("fork");
+  else if (pid) {
+    int status;
+    waitpid(-1, &status, 0);
+    return status;
+  }
+
+mount("none", root_dir, NULL, MS_PRIVATE | MS_REC, NULL);
+
+  if (chroot(root_dir))
+    die(root_dir);
+
+  if (proc && stat("/proc", &sb) == 0 &&
+      mount("proc", "/proc", "proc", 0, NULL))
+    die("/proc");
+
+  if (dev && stat("/dev", &sb) == 0 &&
+      mount("devtmpfs", "/dev", "devtmpfs", 0, "mode=755"))
+    die("/dev");
+
+  if (devpts && stat("/dev/pts", &sb) == 0 &&
+      mount("devpts", "/dev/pts", "devpts", MS_NOEXEC,
+            "gid=5,mode=620,ptmxmode=666"))
+    die("/dev/pts");
+
+  if (sys && stat("/sys", &sb) == 0 && mount("none", "/sys", "sysfs", 0, NULL))
+    die("/sys");
+
+  if (tmp && stat("/tmp", &sb) == 0) {
+    if (mount("tmpfs", "/tmp", "tmpfs", 0, NULL))
+      die("/tmp");
+    if (run && stat("/run", &sb) == 0 &&
+        mkdir("/tmp/run", S_IRWXU | S_IRWXG | S_IRWXO) == 0) {
+      if (mount("/tmp/run", "/run", "bind", MS_BIND | MS_PRIVATE, NULL))
+        die("/run");
+      mkdir("/run/user", S_IRWXU | S_IRWXG);
+    }
+  }
+
+  char *user_runtime_dir = NULL;
+  if (run && stat("/run/user", &sb) == 0) {
+    asprintf(&user_runtime_dir, "/run/user/%d", uid);
+    mkdir(user_runtime_dir, S_IRWXU | S_IRWXG);
+    chown(user_runtime_dir, uid, gid);
+  }
+
+  if (setgid(gid) || setuid(uid))
+    die("setuid");
+
+  struct passwd *pwd;
+  pwd = getpwuid(uid);
+
+  const char *term = getenv("TERM");
+  if (clear_env)
+    clearenv();
+
+  for (int index = 0; index < env_count; index++)
+    putenv(env[index]);
+
+  if (pwd) {
+    setenv("USER", pwd->pw_name, 0);
+    if (stat(pwd->pw_dir, &sb))
+      mkdir(pwd->pw_dir, S_IRWXU);
+    setenv("HOME", pwd->pw_dir, 0);
+    setenv("SHELL", pwd->pw_shell, 0);
+  }
+
+  setenv("TERM", term, 0);
+  setenv("PATH", uid ? "/bin:/usr/bin" : "/bin:/usr/bin:/sbin:/usr/sbin", 0);
+  setenv("SHELL", "/bin/sh", 0);
+
+  if (user_runtime_dir) {
+    setenv("XDG_RUNTIME_DIR", user_runtime_dir, 0);
+    if (stat("/var/chrome/wayland-0", &sb) == 0) {
+      char *file;
+      asprintf(&file, "%s/wayland-0", user_runtime_dir);
+      symlink("/var/chrome/wayland-0", file);
+    }
+  }
+
+  chdir(ch_dir);
+
+  if (argc > optind) {
+    execvp(argv[optind], &argv[optind]);
+    die(argv[optind]);
+  } else {
+    execlp("sh", "sh", NULL);
+    die("sh");
+  }
 }
